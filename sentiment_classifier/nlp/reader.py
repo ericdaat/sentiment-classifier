31cea1be740b542e47478c3eebb75d12d709ccdf
Eric Daoud <daoudattoyan.eric@gmail.com>
2019-03-07 21:47:21 +0100

    test and fix reader

--- sentiment_classifier/nlp/reader.py
+++ sentiment_classifier/nlp/reader.py
@@ -1,26 +1,25 @@
 import os
 import pandas as pd
 from glob import glob
-import logging
+import io
 
 
 class IMDBReader(object):
-    def __init__(self, path):
+    def __init__(self):
         self.train_data = None
         self.test_data = None
 
-        train_data, test_data = self._load_dataset(path)
-        self.train_data = train_data
-        self.test_data = test_data
-
-    def _read_folder(self, path, label):
+    def _read_folder(self, path, label, limit):
         texts = []
-        for file in glob(os.path.join(path, "*.txt")):
-            with open(file, 'r') as f:
+        files = glob(os.path.join(path, "*.txt"))
+        if limit:
+            files = files[:limit]
+
+        for i, file in enumerate(files):
+            with io.open(file, "r", encoding="utf8") as f:
                 text = f.read()
                 texts.append((text, label))
-            break
-                
+
         return texts
 
     def _concat_and_shuffle_dataset(self, pos, neg):
@@ -32,13 +31,14 @@ class IMDBReader(object):
 
         return df
 
-    def _load_dataset(self, path):
-        train_pos = self._read_folder(os.path.join(path, "train", "pos"), 1)
-        train_neg = self._read_folder(os.path.join(path, "train", "neg"), 0)
-        test_pos = self._read_folder(os.path.join(path, "test", "pos"), 1)
-        test_neg = self._read_folder(os.path.join(path, "test", "neg"), 0)
+    def load_dataset(self, path, limit=None):
+        train_pos = self._read_folder(os.path.join(path, "train", "pos"), 1, limit)
+        train_neg = self._read_folder(os.path.join(path, "train", "neg"), 0, limit)
+        test_pos = self._read_folder(os.path.join(path, "test", "pos"), 1, limit)
+        test_neg = self._read_folder(os.path.join(path, "test", "neg"), 0, limit)
 
         train_data = self._concat_and_shuffle_dataset(train_pos, train_neg)
         test_data = self._concat_and_shuffle_dataset(test_pos, test_neg)
 
-        return train_data, test_data
+        self.train_data = train_data
+        self.test_data = test_data
--- tests/tests_reader.py
+++ tests/tests_reader.py
@@ -1,5 +1,29 @@
+import unittest
+import pandas as pd
 from sentiment_classifier.nlp import reader
 
-imdb = reader.IMDBReader("data/aclImdb")
 
-print(imdb.train_data)
+class TestImdb(unittest.TestCase):
+    imdb = reader.IMDBReader()
+
+    def test_init(self):
+        self.assertEqual(TestImdb.imdb.train_data, None)
+        self.assertEqual(TestImdb.imdb.test_data, None)
+
+    def test_read_folder(self):
+        limit = 5
+        TestImdb.imdb.load_dataset("data/aclImdb", limit=limit)
+
+        # test type
+        self.assertIsInstance(TestImdb.imdb.train_data, pd.DataFrame)
+        self.assertIsInstance(TestImdb.imdb.test_data, pd.DataFrame)
+
+        # test shape
+        self.assertEqual(TestImdb.imdb.train_data.shape, (2*limit, 2))
+        self.assertEqual(TestImdb.imdb.test_data.shape, (2*limit, 2))
+
+        # test columns
+        self.assertListEqual(TestImdb.imdb.train_data.columns.tolist(),
+                             ["review", "label"])
+        self.assertListEqual(TestImdb.imdb.test_data.columns.tolist(),
+                             ["review", "label"])
