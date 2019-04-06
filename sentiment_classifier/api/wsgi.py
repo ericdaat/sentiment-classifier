82e937d24be201f86946a924216ab0115f5afff0
Eric <daoudattoyan.eric@gmail.com>
2019-04-06 12:02:39 +0200

    fix bug

--- sentiment_classifier/api/wsgi.py
+++ sentiment_classifier/api/wsgi.py
@@ -1,6 +1,7 @@
 from api import create_app
 from waitress import serve
+from config import PROD_MODEL_FILEPATH
 
 if __name__ == "__main__":
-    application = create_app()
+    application = create_app(model_filepath=PROD_MODEL_FILEPATH)
     serve(application, port=8000)
